218c59ea0f0b95c7ac3ea173b0626495244993ad
GabrielEValenzuela <gabriel.valenzuela@mi.unc.edu.ar>
2024-07-25 20:14:44 -0300

    CMD Line Args (#20)
    * CL:
    
    - Add cmdLineArgs logic.
    
    * CL:
    
    - Add missing endline.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,40 @@
  * MIT License
  */
 
-int main()
+#include "cmdLineArgs.hpp"
+
+int main(const int argc, const char* argv[])
 {
-    // ToDo: Add logic to receive a JSON file and run the system.
+    // LCOV_EXCL_START
+    try
+    {
+        CmdLineArgs cmdLineArgs(argc, argv);
+        auto jsonInput = cmdLineArgs.inputFileJson();
+
+        if (jsonInput.empty())
+        {
+            CmdLineArgs::showHelp();
+            return 1;
+        }
+
+        std::ifstream inputFile(jsonInput);
+        if (!inputFile.is_open())
+        {
+            std::cerr << "Error: Unable to open file " << jsonInput << ". Did you check if the file exists? "
+                      << std::endl;
+            return 1;
+        }
+
+        nlohmann::json json;
+        inputFile >> json;
+        inputFile.close();
+        std::cout << "Running ComfyChairCpp with input file: " << jsonInput << std::endl;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    // LCOV_EXCL_STOP
+    return 0;
 }
